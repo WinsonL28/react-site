4b11586618bd40ce57014a81452df6a797ee7177
WinsonL28 <winsonliang@live.com>
2024-06-22 10:54:17 -0700

    Completed navbar look

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -4,24 +4,26 @@ import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "f
 
 const CNavBar: React.FC = () => {
     return (
-        <Navbar fluid rounded>
-            <NavbarBrand as={Link} href="https://flowbite-react.com">
-                <img src="/favicon.ico" className="bg-black mr-3 h-6 sm:h-9" alt="" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-            </NavbarBrand>
-            <NavbarToggle />
-            <NavbarCollapse>
-                <NavbarLink href="#" active>
-                    Home
-                </NavbarLink>
-                <NavbarLink as={Link} href="#">
-                    About
-                </NavbarLink>
-                <NavbarLink href="#">Services</NavbarLink>
-                <NavbarLink href="#">Pricing</NavbarLink>
-                <NavbarLink href="#">Contact</NavbarLink>
-            </NavbarCollapse>
-        </Navbar>
+        <div className="fixed top-0 left-0 w-full z-20 h-5 ">
+            <Navbar fluid className="bg-black">
+                <NavbarBrand as={Link} href="https://flowbite-react.com">
+                    <img src="/favicon.ico" className="bg-black ml-4 mr-1 h-12 " alt="" />
+                    <span className="self-center whitespace-nowrap text-xl text-white font-semibold p-4">Western Pacific <br/> Robotics Academy</span>
+                </NavbarBrand>
+                <NavbarToggle />
+                <NavbarCollapse>
+                    <NavbarLink href="#" active>
+                        Home
+                    </NavbarLink>
+                    <NavbarLink as={Link} href="#">
+                        About
+                    </NavbarLink>
+                    <NavbarLink href="#">Services</NavbarLink>
+                    <NavbarLink href="#">Pricing</NavbarLink>
+                    <NavbarLink href="#">Contact</NavbarLink>
+                </NavbarCollapse>
+            </Navbar>
+        </div>
     );
 }
 
