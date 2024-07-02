bc3363c1becb8d2e1c320bf6b05928185746619a
WinsonL28 <winsonliang@live.com>
2024-07-02 11:55:41 -0700

    Add darkmode and small navbar changes

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -4,23 +4,21 @@ import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "f
 
 const CNavBar: React.FC = () => {
     return (
-        <div className="fixed top-0 left-0 w-full z-20 h-5 ">
-            <Navbar fluid className="bg-black">
-                <NavbarBrand as={Link} href="https://flowbite-react.com">
-                    <img src="/favicon.ico" className="bg-black ml-4 mr-1 h-12 " alt="" />
-                    <span className="self-center whitespace-nowrap text-xl text-white font-semibold p-4">Western Pacific <br/> Robotics Academy</span>
+        <div className="fixed top-0 w-full z-20 h-5 ">
+            <Navbar fluid className="">
+                <NavbarBrand as={Link} href="#" className="">
+                    <img src="/favicon.ico" className=" ml-4 mr-1 h-[50px] " alt="" />
+                    <span className="self-center whitespace-nowrap text-xl text-white font-semibold p-4">Western Pacific <br /> Robotics Academy</span>
                 </NavbarBrand>
-                <NavbarToggle />
-                <NavbarCollapse>
-                    <NavbarLink href="#" active>
-                        Home
-                    </NavbarLink>
-                    <NavbarLink as={Link} href="#">
-                        About
-                    </NavbarLink>
-                    <NavbarLink href="#">Services</NavbarLink>
-                    <NavbarLink href="#">Pricing</NavbarLink>
-                    <NavbarLink href="#">Contact</NavbarLink>
+                <div>
+                    <NavbarToggle className="hover: bg-gray-700 dark:focus:bg-gray-800 " />
+                </div>
+                <NavbarCollapse className="rounded-md">
+                    <NavbarLink href="#" active     className="text-xl md:text-l rounded-md mx-5">Home</NavbarLink>
+                    <NavbarLink as={Link} href="#"  className="text-xl md:text-l rounded-md mx-5">About</NavbarLink>
+                    <NavbarLink href="#"            className="text-xl md:text-l rounded-md mx-5">Services</NavbarLink>
+                    <NavbarLink href="#"            className="text-xl md:text-l rounded-md mx-5">Pricing</NavbarLink>
+                    <NavbarLink href="#"            className="text-xl md:text-l rounded-md mx-5">Contact</NavbarLink>
                 </NavbarCollapse>
             </Navbar>
         </div>
